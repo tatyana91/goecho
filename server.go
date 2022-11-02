17511f5914d5a6ab82cd88c2fa465527c94dfdcc
tatyana <yata-2012@mail.ru>
2022-11-02 15:04:19 +0300

    Add echo

---
+++ server.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"	
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/health/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "OK")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
